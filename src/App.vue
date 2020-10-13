bd8265857da07dc264f65af8e74368c9fe0f49ef
Natalia <naty.jimenez@gmail.com>
2020-10-12 22:42:58 -0300

    CRUD finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,157 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- Template Modal "Editar" -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-body">
+            <table>
+              <tr>
+                <td>Nombre</td>
+                <td><input v-model="nameEdit" type="text" /></td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td><input v-model="emailEdit" type="text" /></td>
+              </tr>
+              <button @click="update" class="btn btn-primary">Editar</button>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <!-- Sección "Agregar" -->
+    <table class="addTable">
+      <tr>
+        <td>Nombre</td>
+        <td><input v-model="name" type="text" /></td>
+      </tr>
+      <tr>
+        <td>Email</td>
+        <td><input v-model="email" type="text" /></td>
+      </tr>
+      <button @click="create" class="btnAdd btn btn-primary">Agregar</button>
+    </table>
+
+    <!-- Tabla que muestra datos ingresados -->
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Email</th>
+          <th scope="col">Acción</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, i) in users" :key="i">
+          <th scope="row">{{ i + 1 }}</th>
+          <td>{{ user.data.name }}</td>
+          <td>{{ user.data.email }}</td>
+          <td>
+            <button
+              data-toggle="modal"
+              data-target="#exampleModal"
+              @click="setInformation(user.data.name, user.data.email, user.id)"
+              class="btnEdit btn btn-primary"
+            >
+              Editar
+            </button>
+            <button @click="Delete(user.id)" class="btnDelete btn btn-danger">
+              Eliminar
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import firebase from "firebase";
+export default {
+  data() {
+    return {
+      loading: true,
+      users: [],
+      name: "",
+      email: "",
+      nameEdit: "",
+      emailEdit: "",
+      idEdit: "",
+    };
+  },
+  mounted() {
+    this.read();
+  },
+  methods: {
+    setInformation(name, email, id) {
+      this.nameEdit = name;
+      this.emailEdit = email;
+      this.idEdit = id;
+    },
+    // Leer Data
+    read() {
+      firebase
+        .firestore()
+        .collection("users")
+        .onSnapshot((snapshot) => {
+          this.users = [];
+          snapshot.forEach((doc) => {
+            console.log(doc.id, "=>", doc.data());
+            this.users.push({
+              id: doc.id,
+              data: doc.data(),
+            });
+          });
+        });
+    },
+    // Crear Data
+    create() {
+      firebase
+        .firestore()
+        .collection("users")
+        .add({
+          name: this.name,
+          email: this.email,
+        })
+        .then(() => {
+          snapshot.forEach((doc) => {
+            console.log("data enviada");
+          });
+        });
+    },
+    // Editar Data
+    update() {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(this.idEdit)
+        .update({
+          name: this.nameEdit,
+          email: this.emailEdit,
+        })
+        .then(() => {
+          console.log("Data enviada");
+        });
+    },
+    // Eliminar Data
+    Delete(id) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(id)
+        .delete();
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -16,7 +161,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 
@@ -28,5 +172,26 @@
       color: #42b983;
     }
   }
+}
+.modal-body {
+  display: flex;
+  align-items: center;
+}
+.addTable {
+  margin-top: 50px;
+  margin-bottom: 50px;
+
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+.btnEdit {
+  margin-right: 5px;
+  padding-left: 20px !important;
+  padding-right: 20px !important;
+}
+.btnDelete {
+  margin-right: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCduIVc6w8-jxWEEhjAaWt9cs5ohB8ZDiw",
+  authDomain: "auth-vrouter.firebaseapp.com",
+  databaseURL: "https://auth-vrouter.firebaseio.com",
+  projectId: "auth-vrouter",
+  storageBucket: "auth-vrouter.appspot.com",
+  messagingSenderId: "1023828134464",
+  appId: "1:1023828134464:web:9e01780143f6e87ef4ada5"
+};
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
